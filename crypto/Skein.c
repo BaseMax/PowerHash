874b8bccd8bfe9779ac3382dc013facc6dfe5c64
Max Base <MaxBaseCode@Gmail.Com>
2018-08-12 16:53:25 +0430

    Update Skein.c

--- crypto/Skein.c
+++ crypto/Skein.c
@@ -268,34 +268,26 @@ static inline void memcpy_swap64(void *dst, const void *src, size_t n)
 #endif
 #ifndef Skein_Put64_LSB_First
 void    Skein_Put64_LSB_First(uint8_t *dst,const u64b_t *src,size_t bCnt)
-#ifdef SKEIN_PORT_CODE
-	{
+{
 	size_t n;
 	for(n=0;n<bCnt;n++)
 		dst[n] = (uint8_t) (src[n>>3] >> (8*(n&7)));
-	}
-#else
-	;
-#endif
+}
 #endif
 #ifndef Skein_Get64_LSB_First
 	void    Skein_Get64_LSB_First(u64b_t *dst,const uint8_t *src,size_t wCnt)
-#ifdef SKEIN_PORT_CODE
-{
-	size_t n;
-	for(n=0;n<8*wCnt;n+=8)
-		dst[n/8] = (((u64b_t) src[n  ])      ) +
-				   (((u64b_t) src[n+1]) <<  8) +
-				   (((u64b_t) src[n+2]) << 16) +
-				   (((u64b_t) src[n+3]) << 24) +
-				   (((u64b_t) src[n+4]) << 32) +
-				   (((u64b_t) src[n+5]) << 40) +
-				   (((u64b_t) src[n+6]) << 48) +
-				   (((u64b_t) src[n+7]) << 56) ;
-	}
-#else
-	;
-#endif
+	{
+		size_t n;
+		for(n=0;n<8*wCnt;n+=8)
+			dst[n/8] = (((u64b_t) src[n  ])      ) +
+					   (((u64b_t) src[n+1]) <<  8) +
+					   (((u64b_t) src[n+2]) << 16) +
+					   (((u64b_t) src[n+3]) << 24) +
+					   (((u64b_t) src[n+4]) << 32) +
+					   (((u64b_t) src[n+5]) << 40) +
+					   (((u64b_t) src[n+6]) << 48) +
+					   (((u64b_t) src[n+7]) << 56) ;
+		}
 #endif
 
 
